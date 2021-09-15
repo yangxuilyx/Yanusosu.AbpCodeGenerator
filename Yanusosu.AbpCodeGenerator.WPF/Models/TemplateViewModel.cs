bbb4acf908644e53cfd97e3bb7803cd8eef191ea
杨绪 <yangxuilyx@hotmail.com>
2021-09-16 00:03:41 +0800

    更新

--- Yanusosu.AbpCodeGenerator.WPF/Models/TemplateViewModel.cs
+++ Yanusosu.AbpCodeGenerator.WPF/Models/TemplateViewModel.cs
@@ -20,54 +20,54 @@ namespace Yanusosu.AbpCodeGenerator.WPF.Models
 
             return new List<TemplateViewModel>()
             {
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain","EntityManager.txt"),
-                    SavePath = Path.Combine(model.FilePath,@"_GenerateCode\Modules\Domain\",module.Names),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}",$"{module.Name}Manager.cs"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain","README.txt"),
-                    SavePath = Path.Combine(model.FilePath,@"_GenerateCode\Modules\Domain\",module.Names),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}","README.md"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain\Authorization","EntityAppPermissions.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization",$"{model.ClassName}AppPermissions.cs"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain\Authorization","EntityAuthorizationProvider.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization",$"{model.ClassName}AuthorizationProvider.cs"),
-                },
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain","EntityManager.txt"),
+                //    SavePath = Path.Combine(model.FilePath,@"_GenerateCode\Modules\Domain\",module.Names),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}",$"{module.Name}Manager.cs"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain","README.txt"),
+                //    SavePath = Path.Combine(model.FilePath,@"_GenerateCode\Modules\Domain\",module.Names),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}","README.md"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain\Authorization","EntityAppPermissions.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization",$"{model.ClassName}AppPermissions.cs"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Domain\Authorization","EntityAuthorizationProvider.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Domain\{module.Names}\Authorization",$"{model.ClassName}AuthorizationProvider.cs"),
+                //},
                 // Application 
                 new TemplateViewModel()
                 {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dto","PagedEntityResultRequestDto.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto",$"Paged{model.ClassName}ResultRequestDto.cs"),
+                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dtos","PagedEntityInput.txt"),
+                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos"),
+                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos",$"Paged{model.ClassName}Input.cs"),
                 },
                 new TemplateViewModel()
                 {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dto","EntityDto.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto",$"{model.ClassName}Dto.cs"),
+                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dtos","EntityDto.txt"),
+                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos"),
+                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos",$"{model.ClassName}Dto.cs"),
                 },
                 new TemplateViewModel()
                 {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dto","CreateEntityDto.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto",$"Create{model.ClassName}Dto.cs"),
+                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dtos","CreateEntityDto.txt"),
+                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos"),
+                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos",$"Create{model.ClassName}Dto.cs"),
                 },
                 new TemplateViewModel()
                 {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dto","UpdateEntityDto.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dto",$"Update{model.ClassName}Dto.cs"),
+                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\Application\Dtos","UpdateEntityDto.txt"),
+                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos"),
+                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}\Dtos",$"Update{model.ClassName}Dto.cs"),
                 },
                 new TemplateViewModel()
                 {
@@ -81,45 +81,45 @@ namespace Yanusosu.AbpCodeGenerator.WPF.Models
                     SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}"),
                     FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\Application\{module.Names}",$"{model.ClassName}AppService.cs"),
                 },
-                // EntityFrameworkCore
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\Modules\EntityFrameworkCore\Configurations","EntityCfg.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\EntityFrameworkCore\Configurations\{module.Names}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\EntityFrameworkCore\Configurations\{module.Names}",$"{model.ClassName}cfg.cs"),
-                },
+                //// EntityFrameworkCore
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\Modules\EntityFrameworkCore\Configurations","EntityCfg.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\EntityFrameworkCore\Configurations\{module.Names}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\Modules\EntityFrameworkCore\Configurations\{module.Names}",$"{model.ClassName}cfg.cs"),
+                //},
 
-                // vue
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\vue\api","entity.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\api\{module.ModuleSplitName}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\api\{module.ModuleSplitName}",$"{module.SplitName}.ts"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","entity.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"index.vue"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","create-entity.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"create-{module.SplitName}.vue"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","update-entity.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"update-{module.SplitName}.vue"),
-                },
-                new TemplateViewModel()
-                {
-                    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","README.txt"),
-                    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
-                    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"README.md"),
-                },
+                //// vue
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\vue\api","entity.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\api\{module.ModuleSplitName}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\api\{module.ModuleSplitName}",$"{module.SplitName}.ts"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","entity.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"index.vue"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","create-entity.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"create-{module.SplitName}.vue"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","update-entity.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"update-{module.SplitName}.vue"),
+                //},
+                //new TemplateViewModel()
+                //{
+                //    TemplatePath = Path.Combine(basePath,@"Templates\vue\views\entities","README.txt"),
+                //    SavePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}"),
+                //    FilePath = Path.Combine(model.FilePath,$@"_GenerateCode\vue\views\{module.ModuleSplitName}\{module.SplitName}",$"README.md"),
+                //},
             };
         }
     }
